27fe067a398441ab6fd66c7980626e36ffc8cb99
Connor <cguy33333@gmail.com>
2024-04-09 12:02:53 -0700

    added the main logic for our website, we now generate a random quote and replace words inside of it to randomized words. Pushing to main because this is necessary for every branch.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,92 +7,68 @@ const obj = {
 
 let randomQuote =``;
 
-function getContent() {
-    fetch(quotesURL, obj).then(response => response.json().then(function (data) {
-        randomizeQuote(data[0].content);
-      }));
+function generatePromises(quoteArray) {
+    // this becomes an array of functions -->
+    let promises = [];
+
+    for (let i=0; i<quoteArray.length; i++) {
+        if (quoteArray[i].length > 4) {
+            quoteArray[i] = `randomize`;
+            // <-- here we pass the function we want to run (but its time consuming)
+            promises.push(getWord());
+        } 
+    }
+
+    // here when we return the Promise.all we run the array of functions, running the function
+    // we passed as many indexes as are in the array of functions above.
+    return Promise.all(promises);
 }
 
 function randomizeQuote(randomQuote) {
     console.log(randomQuote);
-    // console.log(randomQuote.length);
 
-    //for loop that grabs every word
     const quoteArray = randomQuote.split(" ");
-    // console.log(quoteArray);
-    let iterations = 0;
-    // this should be reworked we dont need an else 
-    // actually this whole for loop is deprecated, im leaving it in for now though since its part of the logic technically
-    for (let i=0; i<quoteArray.length; i++) {
-        if (quoteArray[i].length <= 4) {
-            // nothing
-        } else {
-            iterations++;
+    // here we are grabbing said data from the promise, similar to how you would a fetch because
+    // a fetch is technically a promise.
+    generatePromises(quoteArray).then(newWords => {
+        let newWordCount = 0;
+        for (let i=0; i<quoteArray.length; i++) {
+            if (quoteArray[i] === `randomize`) {
+                quoteArray[i] = newWords[newWordCount];
+                newWordCount++;
+            }
         }
-    }
+        console.log(quoteArray);
 
-    let randomPercent = randomInt(iterations + 1);
+        reCreateString(quoteArray);
+    });
+}
 
-    const placeholder = [];
+function reCreateString(quoteArray) {
+    let sentence = "";
     for (let i=0; i<quoteArray.length; i++) {
-        if (quoteArray[i].length > 4) {
-            placeholder.push(getWordContent());
-        } 
+        sentence += `${quoteArray[i]} `;
     }
-    console.log(placeholder);
-    // this is no random now
-    // i think the issue is here, watch what happens when i run the code and notice how slowly the new words load 
-    // it might not be able to apply them fast enough? so we might need some sort of buffer, but im unsure
-    for (let i=0; i<quoteArray.length; i++) {
-        if (quoteArray[i].length > 4) {
-            quoteArray[i] = `${getWordContent()}`;
+    console.log(sentence);
 
-        } 
-    }
-
-    console.log(quoteArray);
-    // console.log(randomPercent);
+    return sentence;
 }
 
-function getWordContent() {
-    fetch(wordsURL, obj).then(response => response.json().then(function (data) {
-        word(data[0]);
-      }));
+function getQuote() {
+    fetch(quotesURL, obj).then(response => response.json()).then(function (data) {
+        randomizeQuote(data[0].content);
+      });
 }
 
-function word(word) {
-    console.log(word); //yes
-    // this would be returning a string at this point
-    return word;
+function getWord() {
+    let word;
+    return fetch(wordsURL, obj).then(response => response.json()).then(function (data) {
+        // word(data[0]);
+        word = data[0];
+        // console.log(word);
+        return word;
+      });
 }
 
-function randomInt(n) { 
-    return Math.floor(Math.random() * n)
-}
-
-// take content and clean the data, trim() need number of words
-
-
-// random % chance to select random number of words --> words >4 
-
-
-
-// random chance will be based on the number of larger words that we can replace
-
-
-
-// decide which words will be randomized in a few different ways 
-
-
-
-
-
-
-
-
-
-
-
-
-
-div.addEventListener(`click`, getContent);
+div.addEventListener(`click`, getQuote);
+// MORE JAVASCRIPT GOES HERE 
